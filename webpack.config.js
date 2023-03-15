0ac582561e440862e77592cab7d21463473d214a
jhpaige <jhpaige@usc.edu>
2023-03-15 16:54:17 -0700

    Edited webpack to allow css and file loading

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,18 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     ],
   },
   // pass all js files through Babel

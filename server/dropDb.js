8abc3392161fef18e4e9ec0e35da14802ca5ffd5
jhpaige <jhpaige@usc.edu>
2023-03-15 19:55:52 -0700

    Added drop table query logic

--- server/dropDb.js
+++ server/dropDb.js
@@ -1,1 +1,21 @@
-// Write SQL to drop everything in DB
+// SQL to drop everything in DB
+
+const db = require('./swatchifyModel');
+
+// Dropping tables from database to reset
+const queryString = `
+  DROP TABLE labels;
+  DROP TABLE colors;
+`;
+
+db.query(queryString).then((data) => {
+  if (!data.rows.length){
+    console.log('dropping successful');
+    return next();
+  }
+  return next();
+}).catch((err) => next({
+  log: `Error in dropDb: ${err}`,
+  status: 400,
+  message: 'Query to drop tables unsuccessful, check server',
+}));

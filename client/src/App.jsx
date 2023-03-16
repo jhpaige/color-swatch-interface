5c2bdb2bf99e1f032c18ecce01fdec292bbf253f
jhpaige <jhpaige@usc.edu>
2023-03-16 12:21:20 -0700

    Successfully loaded in data from server/database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import MainPage from './MainPage';
 import './App.css';
 
 const App = () => {
+
+  const [ allColors, setAllColors ] = useState([]);
+  const [ selectedColors, setSelectedColors ] = useState([]);
+
+  const fetchColors = async () => {
+    try {
+      let response = await fetch('http://localhost:3001/colors');
+      let json = await response.json();
+      setAllColors(json);
+      return { success: true, data: json };
+    } catch (error) {
+      console.log(error);
+      return { success: false };
+    }
+  }
+
+  fetchColors(setAllColors);
+
   return (
     <div className="wrapper">
+      {JSON.stringify(allColors)}
       <Navbar />
       <div>
         <Sidebar />

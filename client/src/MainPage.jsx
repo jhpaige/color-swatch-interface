63456bbbaf8135ae9075711ed1e5d70074856f43
jhpaige <jhpaige@usc.edu>
2023-03-16 15:42:40 -0700

    Color swatches now loading in

--- client/src/MainPage.jsx
+++ client/src/MainPage.jsx
@@ -1,20 +1,21 @@
 import { Container } from '@mui/material';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ColorSwatch from './ColorSwatch';
 
 const MainPage = ({ allColors }) => {
 
+  const [pageSwatches, setPageSwatches] = useState([]);
 
-
-  useEffect(() => {
-    console.log(allColors);
-  }, [ allColors ])
+  // Sets initial value for page swatches
+  if (pageSwatches.length == 0 && allColors.length > 0) {
+    setPageSwatches(allColors);
+  }
 
   return (
     <Container id="mainpage-container" sx={{ height: '100%' }}>
       {JSON.stringify(allColors)}
-      {allColors.map(color => {
-        <ColorSwatch key={color.code} colorCode={color.code} />
+      {pageSwatches.map(color => {
+        return (<ColorSwatch key={color.code} colorCode={color.code} />)
       })}
     </Container>
   )

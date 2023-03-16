f8dfb866386492e7f3a81369b2439820c0ea2d02
jhpaige <jhpaige@usc.edu>
2023-03-16 15:29:34 -0700

    Implemented navbar buttons and styling

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import './Navbar.css';
+import { Container, Box } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <div className="wrapper">
-      
-    </div>
+    <Container id="navbar-container" disableGutters sx={{
+      backgroundColor: 'rgb(54, 59, 59)',
+      width: '100%',
+      height: 'fit-content',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingRight: '10px'
+    }}>
+      <img src="https://assets.website-files.com/6036a858eff6dd0e0891eac0/605b81c28fb6ff9fe4461f25_HH_Logo_Icon_White.svg" loading="lazy" data-w-id="8f972a32-c567-8b86-8967-2f40893a39e6" alt="Helpful Human Logo" className="hh-logo"/>
+      <TextField
+        label=""
+        placeholder="Search"
+        id="outlined-size-small"
+        size="small"
+        sx={{m: 1.5}}
+      />
+    </Container>
   )
 }
 

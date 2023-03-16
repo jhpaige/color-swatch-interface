085e50797cb0041d13b3d9c1ece3ac3c8bb8fac0
jhpaige <jhpaige@usc.edu>
2023-03-16 15:30:37 -0700

    Added button functionality and styling for sidebar

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Container, Button, Box } from '@mui/material';
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div className="wrapper">
-      
-    </div>
+    <Box boxShadow='2' sx={{borderRight: 0.2, borderColor: "primary.main"}}>
+      <Container id="sidebar-container" disableGutters sx={{
+        backgroundColor: 'rgb(214, 216, 216)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: 'fit-content',
+        height: '100%'
+      }}>
+        <Button variant="outlined" sx={{
+          backgroundColor: 'white',
+          margin: '20px',
+          marginTop: '40px',
+          whiteSpace: 'nowrap',
+          paddingLeft: '25px',
+          paddingRight: '25px'
+        }}>
+          Random Color
+        </Button>
+        <Container sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'left',
+          width: '100%',
+          Button: {
+            justifyContent: 'left',
+            padding: '0px',
+            paddingLeft: '5px',
+          }
+        }}>
+          <Button>Red</Button>
+          <Button>Orange</Button>
+          <Button>Yellow</Button>
+          <Button>Green</Button>
+          <Button>Blue</Button>
+          <Button>Purple</Button>
+          <Button>Brown</Button>
+          <Button>Gray</Button>
+        </Container>
+      </Container>
+    </Box>
+    
   )
 }
 

123537bc616044840f960c94c49bdf089f69869b
jhpaige <jhpaige@usc.edu>
2023-03-16 17:08:52 -0700

    Added styling and hex code info for color swatch

--- client/src/ColorSwatch.jsx
+++ client/src/ColorSwatch.jsx
@@ -1,11 +1,18 @@
-import { Container, Box } from '@mui/material';
+import { Container, Card, Box, Text} from '@mui/material';
 import React from 'react';
 
 const ColorSwatch = ({ colorCode }) => {
   return (
-    <Container id="color-palette-container">
-      <Box sx={{height: '70px', width: '70px', backgroundColor: `#${colorCode}`}}/>
-    </Container>
+    <Card id="color-palette-box" sx={{
+      '&:hover': { boxShadow: 5, cursor: 'pointer' },
+    }}>
+      <Box sx={{
+        height: '120px',
+        width: '120px',
+        backgroundColor: `#${colorCode}`
+      }}/>
+      <Box sx={{padding: '10px'}}>#{colorCode}</Box>
+    </Card>
   )
 }
 

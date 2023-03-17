70c0ae534a4f855d5729f9973c8d9b65713d7f9a
jhpaige <jhpaige@usc.edu>
2023-03-17 09:38:12 -0700

    Added remaining color codes to database

--- server/addToDb.js
+++ server/addToDb.js
@@ -44,15 +44,15 @@ const addToDb = async () => {
   // Storing labels data in array and converts to query string
   let labelsValuesString = '';
   let currColorId = 1;
-  const redLabelCodes = ['D0312D','990F02','E3242B','60100B','610C04','B90E0A','900603','900D09','4E0707','7E2811','A91B0D','710C04','5E1916'];
-  const orangeLabelCodes = ['ED7014','FA8128','FCAE1E','B56727','8D4004','BE5504','FC6A03','DD571C','B2560D','FDA172','ED820E','80400B','EC9706'];
-  const yellowLabelCodes = [];
-  const greenLabelCodes = [];
-  const blueLabelCodes = [];
-  const purpleLabelCodes = [];
-  const brownLabelCodes = [];
-  const grayLabelCodes = [];
-  const labelCodes = [...redLabelCodes, ...orangeLabelCodes];
+  const redLabelCodes = ['d0312d','990f02','e3242b','60100b','610c04','b90e0a','900603','900d09','4e0707','7e2811','a91b0d','710c04','5e1916'];
+  const orangeLabelCodes = ['ed7014','fa8128','fcae1e','b56727','8d4004','be5504','fc6a03','dd571c','b2560d','fda172','ed820e','80400b','ec9706'];
+  const yellowLabelCodes = ['fec20c', 'fca510', 'febe00', 'ffd300', 'fdd128', 'dffe00', 'ffef00', 'fefbbd', 'fee135', 'cdaa6e', 'cc7722', 'f9e075', 'fbe106'];
+  const greenLabelCodes = ['3cb043', 'b0fc38', '3a5311', '728c69', 'aef359', '5dbb63', '98bf64', '028a0f', '74b72e', '466d1d', '03ac13', '3dec97', '234f1e'];
+  const blueLabelCodes = ['3944bc', '63c5da', '757c88', '0a1172', '281e5d', '1338be', '48aaad', '016064', '022d36', '1520a6', '0492c2', '2832c2', '82eefd'];
+  const purpleLabelCodes = ['b200ed', 'b43757', '784b84', 'c64b8c', 'e4a0f7', 'af69ee', 'b660cd', '8f00ff', 'b284be', '6f2da8', '9966cb', '702963', 'b5338a'];
+  const brownLabelCodes = ['231709', '4b371c', '3c280d', '795c34', '362511', '371d10', '3f301d', '4a2511', '432616', '65350f', '5e2c04', '481f02', '2e1503'];
+  const grayLabelCodes = ['828282', '787276', '88807b', 'd9dddc', 'd6cfc7', 'c7c6c1', 'bebdb8', 'bdb7ab', '999da0', '777b7e', '877f7d', '48494b', 'b9bbb6'];
+  const labelCodes = [...redLabelCodes, ...orangeLabelCodes, ...yellowLabelCodes, ...greenLabelCodes, ...blueLabelCodes, ...purpleLabelCodes, ...brownLabelCodes, ...grayLabelCodes];
   // Creates values string for labels insert
   for (let i = 1; i <= labelCodes.length; i++) {
     labelsValuesString += `('${labelCodes[i - 1]}', ${currColorId})`;
